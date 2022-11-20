28e5a514dec8f43dd3a73a70ae0a7eb37461d3f8
COZMIKDX <tacocatdx@gmail.com>
2022-11-20 15:17:47 -0600

    Got all apngs to cycle through. Need to freeze at an idle frame until the next tick

--- src/c/main_window.c
+++ src/c/main_window.c
@@ -6,31 +6,26 @@ static Window *s_window;
 static TextLayer *hour_layer;
 static TextLayer *minute_layer;
 
-static GBitmap *fox_bitmap = NULL;
-static BitmapLayer *bitmap_layer;
-static GBitmapSequence *fox_sequence = NULL;
+static GBitmap *anim_bitmap = NULL;
+static BitmapLayer *anim_bitmap_layer;
+static GBitmapSequence *anim_sequence = NULL;
 
 static GBitmap * idle_bitmap;
 static BitmapLayer *idle_bitmap_layer;
 
-//static const uint32_t RESOURCES [4] = {RESOURCE_ID_FOX_IDLE, RESOURCE_ID_SHEEP_IDLE, RESOURCE_ID_WOLF_IDLE, RESOURCE_ID_BUNNY_IDLE};
+static const uint32_t IDLE_RESOURCES [4] = {RESOURCE_ID_FOX_IDLE, RESOURCE_ID_SHEEP_IDLE, RESOURCE_ID_WOLF_IDLE, RESOURCE_ID_BUNNY_IDLE};
+static const uint32_t RESOURCES[4] = {RESOURCE_ID_FOX, RESOURCE_ID_SHEEP, RESOURCE_ID_WOLF, RESOURCE_ID_BUNNY};
+static uint32_t current_animal = 0;
 
 // currently experimenting
-static uint32_t current_animal = 0;
-static uint32_t starttime = 0;
-static uint32_t currentsec = 0;
-static uint32_t startdelay = 10; //sec
 static bool finishedstart = false;
 
-// Draw the background elements onto the canvas layer.
-static void canvas_update_proc(Layer *layer, GContext *ctx) {
-}
+static void load_sequence();
 
 static void update_time() {
     // Get a tm structure.
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
-    starttime = tick_time->tm_sec;
 
     // format the time from the tm structure and stick it into a buffer
     static char hour_buffer[4];
@@ -57,6 +52,8 @@ static void update_time() {
     
 }*/
 
+
+
 static void tick_handler(struct tm *tick_time, TimeUnits unit_changed) {
     update_time();
     // Do other things.
@@ -64,19 +61,56 @@ static void tick_handler(struct tm *tick_time, TimeUnits unit_changed) {
     //update_animal();
 }
 
-static void timer_handler(void *context) {
+static void timer_handler(void *context)
+{
     uint32_t next_delay;
 
     // Advance to the next APNG frame, and get the delay for this frame
-    if (gbitmap_sequence_update_bitmap_next_frame(fox_sequence, fox_bitmap, &next_delay))
+    if (gbitmap_sequence_update_bitmap_next_frame(anim_sequence, anim_bitmap, &next_delay))
     {
         // Set the new frame into the BitmapLayer
-        bitmap_layer_set_bitmap(bitmap_layer, fox_bitmap);
-        layer_mark_dirty(bitmap_layer_get_layer(bitmap_layer));
+        bitmap_layer_set_bitmap(anim_bitmap_layer, anim_bitmap);
+        layer_mark_dirty(bitmap_layer_get_layer(anim_bitmap_layer));
 
         // Timer for that frame's delay
         app_timer_register(next_delay, timer_handler, NULL);
     }
+    else
+    {
+        // Start again
+        current_animal = current_animal + 1;
+        current_animal = current_animal % 4;
+        load_sequence();
+    }
+}
+
+static void load_sequence()
+{
+    // If you run this function again after the animation stops, this block will reload it.
+    if (anim_sequence)
+    {
+        gbitmap_sequence_destroy(anim_sequence);
+        anim_sequence = NULL;
+    }
+    if (anim_bitmap)
+    {
+        gbitmap_destroy(anim_bitmap);
+        anim_bitmap = NULL;
+    }
+
+    if (idle_bitmap) {
+        gbitmap_destroy(idle_bitmap);
+        idle_bitmap = NULL;
+    }
+
+    idle_bitmap = gbitmap_create_with_resource(IDLE_RESOURCES[current_animal]);
+    bitmap_layer_set_bitmap(idle_bitmap_layer, idle_bitmap);
+
+    anim_sequence = gbitmap_sequence_create_with_resource(RESOURCES[current_animal]);
+    GSize anim_frame_size = gbitmap_sequence_get_bitmap_size(anim_sequence);
+
+    anim_bitmap = gbitmap_create_blank(anim_frame_size, GBitmapFormat8Bit);
+    app_timer_register(1, timer_handler, NULL);
 }
 
 static void window_load(Window *window) {
@@ -89,14 +123,14 @@ static void window_load(Window *window) {
     // layer_set_update_proc(canvas, canvas_update_proc);
     // layer_add_child(window_layer, canvas);
 
-    bitmap_layer = bitmap_layer_create(window_bounds);
-    layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer));
+    idle_bitmap_layer = bitmap_layer_create(layer_get_bounds(window_get_root_layer(window)));
+    layer_add_child(window_layer, bitmap_layer_get_layer(idle_bitmap_layer));
+    bitmap_layer_set_compositing_mode(idle_bitmap_layer, GCompOpSet);
+    bitmap_layer_set_bitmap(idle_bitmap_layer, idle_bitmap);
 
-    fox_sequence = gbitmap_sequence_create_with_resource(RESOURCE_ID_WOLF);
-    GSize fox_frame_size = gbitmap_sequence_get_bitmap_size(fox_sequence);
-
-    fox_bitmap = gbitmap_create_blank(fox_frame_size, GBitmapFormat8Bit);
-    app_timer_register(1, timer_handler, NULL);
+    anim_bitmap_layer = bitmap_layer_create(window_bounds);
+    layer_add_child(window_layer, bitmap_layer_get_layer(anim_bitmap_layer));
+    load_sequence();
 
     hour_layer = text_layer_create(GRect(0, 20, window_bounds.size.w/2, 50));
     text_layer_set_background_color(hour_layer, GColorClear);
@@ -142,12 +176,12 @@ void main_window_create() {
 // Ran in deinit.
 void main_window_destroy() {
     //destroy stuff. Not sure if destroying the window must be done last.
-    // gbitmap_sequence_destroy(fox_sequence);
-    // gbitmap_destroy(fox_bitmap);
-    // bitmap_layer_destroy(bitmap_layer);
+    gbitmap_sequence_destroy(anim_sequence);
+    gbitmap_destroy(anim_bitmap);
+    bitmap_layer_destroy(anim_bitmap_layer);
 
-        gbitmap_destroy(idle_bitmap);
-        bitmap_layer_destroy(idle_bitmap_layer);
+        // gbitmap_destroy(idle_bitmap);
+        // bitmap_layer_destroy(idle_bitmap_layer);
 
     window_destroy(s_window);
 }
